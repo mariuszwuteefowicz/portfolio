4e7ff26fb0bfce5d7747c561ffdea67351fce070
Mariusz <mariusz.c008@protonmail.com>
2023-10-05 10:44:30 +0200

    create mobile styles for header & about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
     extend: {
       colors: {
       bg: '#020916',
-      accent: '#CEFF7E'
+      accent: '#CEFF7E',
+      lightGray: '#1F2937'
       },
       fontFamily: {
         'dmsans': ['DM Sans', 'sans-serif']
+      },
+      maxWidth: {
+        'wtf': '82rem'
       }
     },
   },
